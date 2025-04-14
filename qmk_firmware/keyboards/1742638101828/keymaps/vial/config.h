c4d850f8d0ad0f2428b6dfa2c33cbb4abc569f05
RitHase <clf14097@nitech.jp>
2025-04-14 21:17:33 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/1742638101828/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0x4d, 0x49, 0xa5, 0xb5, 0x5f, 0xc1, 0xec, 0xce}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
